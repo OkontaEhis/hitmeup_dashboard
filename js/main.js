faeaa5b39361bf021fc6c55ff2d271c5f657cf42
Ehijesumuan Michelle  Okonta <okontaehijesumuan@gmail.com>
2025-01-26 21:15:44 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const db = firebase.firestore();
 
 // Form Submission Handler
 document.getElementById("contact-form").addEventListener("submit", async (e) => {
